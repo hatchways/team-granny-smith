10b05e146658d5352c092e90dddd1dd6a875bd54
davidch25 <35306691+davidc25tlumby@users.noreply.github.com>
2021-05-19 12:15:28 -0400

    move hook to inside ProtectedRoute and added logic for when API callback hasn't happened yet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,13 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
-import { AuthProvider, useAuth } from './context/useAuthContext';
+import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.js';
 import './App.css';
 
 function App(): JSX.Element {
-  const { loggedInUser } = useAuth();
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
@@ -21,7 +20,7 @@ function App(): JSX.Element {
               <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
-                <ProtectedRoute exact path="/dashboard" loggedInUser={loggedInUser} component={Dashboard} />
+                <ProtectedRoute exact path="/dashboard" component={Dashboard} />
                 <Route path="*">
                   <Redirect to="/login" />
                 </Route>
--- client/src/components/ProtectedRoute/ProtectedRoute.js
+++ client/src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,25 +1,33 @@
+import { CircularProgress } from '@material-ui/core';
 import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from '../../context/useAuthContext';
 
-//Protected route component to wrap pages that require users to be logged in beforehand.
+//Protected route  component to wrap pages that require users to be logged in beforehand.
 //Redirects to login page is user=false
-const ProtectedRoute = ({ component: Component, loggedInUser, ...rest }) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { loggedInUser, isLoading } = useAuth();
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (loggedInUser) {
-          return <Component {...rest} {...props} />;
+        if (isLoading) {
+          return <CircularProgress />;
         } else {
-          return (
-            <Redirect
-              to={{
-                pathname: '/login',
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
+          console.log(loggedInUser);
+          if (loggedInUser) {
+            return <Component {...rest} {...props} />;
+          } else {
+            return (
+              <Redirect
+                to={{
+                  pathname: '/login',
+                  state: {
+                    from: props.location,
+                  },
+                }}
+              />
+            );
+          }
         }
       }}
     />
