0d1bbcb590358b52a8d789fc1fb6d2a2d8d49aaf
jparengo <parengojulian@gmail.com>
2021-05-27 14:51:04 -0300

    added control for different price ids

--- server/utils/urlScraping.js
+++ server/utils/urlScraping.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const Product = require("../models/Product");
 
 const scrapingAmazon = async (url) => {
   if (!url) return null;
@@ -11,27 +12,45 @@ const scrapingAmazon = async (url) => {
   // Go to Amazon Product page
   try {
     await page.goto(url, {
-      waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"]
+      waitUntil: ["load", "domcontentloaded", "networkidle0", "networkidle2"],
     });
     await page.waitForSelector("#productTitle");
     await page.waitForSelector("#priceblock_ourprice");
 
     // Extract information from page
     const { title, price, imageUrl } = await page.evaluate(() => {
+      let price;
       const title = document.getElementById("productTitle").innerText;
-      const price = document.getElementById("priceblock_ourprice").innerText;
+
+      if (document.getElementById("priceblock_ourprice") != null) {
+        price = document.getElementById("priceblock_ourprice").innerText;
+      } else if (document.getElementById("priceblock_saleprice" != null)) {
+        price = document.getElementById("priceblock_saleprice").innerText;
+      } else {
+        price = "Product Not Available";
+      }
+
       const imageUrl = document
         .getElementById("landingImage")
         .getAttribute("src");
+
       return { title, price, imageUrl };
     });
-    await browser.close();
-    return { title, price, imageUrl };
   } catch (err) {
     console.error(err);
-    await browser.close();
     return null;
   }
+
+  // Create object for return
+  const product = new Product({
+    title,
+    price,
+    imageUrl,
+  });
+
+  await browser.close();
+
+  return product;
 };
 
 module.exports = { scrapingAmazon };
